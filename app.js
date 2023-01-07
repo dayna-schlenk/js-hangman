aa0aa7f71a4c5a82b2fcec2ce83da04ccc1fce71
dayna-schlenk <dayna.schlenker@gmail.com>
2023-01-06 18:25:40 -0600

    Add comments for next steps

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+// ------------------------
+// MVP Requirements
+// ------------------------
 // User starts with 10 lives
 // Generate random word (start with an array of basic words)
 // Empty spaces to populate letters in random mystery word
 // ABC letter buttons for user to select (which will then either populate the correct letter in the proper place, or take away 1 life from the user if incorrect)
 // Game ends when either the user runs out of lives or the word is guessed correctly
 // Button shows up to either play again or quit
+
+
+// ------------------------
+// Next Steps
+// ------------------------
+// Print a random word from an array to the console
+// User clicks on a letter-button, and an "I've been clicked" message appears for all buttons
+    // User clicks on a letter-button, console indicates which letter was clicked
