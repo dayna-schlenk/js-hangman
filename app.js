e9f91dcfde197b74ce9d52f15d2bad97ffb44652
dayna-schlenk <dayna.schlenker@gmail.com>
2023-02-27 07:28:57 -0600

    Add padding to abc buttons

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ function createAbcButtons() {
     for (let letter of alphabet) {
         let newButton = document.createElement("button");
         newButton.innerText = letter;
-        newButton.classList.add("btn", "btn-secondary", "m-2", "abc-btns");
+        newButton.classList.add("btn", "btn-secondary", "m-2", "py-3", "px-4", "abc-btns");
         abcSection.appendChild(newButton);
     }
 }
